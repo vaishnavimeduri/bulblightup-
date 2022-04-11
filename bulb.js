4799ce71ead29910d7cfa0a3ccaa1c9a28199ff7
vaishnavimeduri <meduri.vaishu676@gmail.com>
2022-04-11 19:32:26 +0530

    first commit

---
+++ bulb.js
@@ -0,0 +1,28 @@
+function bulbon(){
+    document.getElementById('firstimg').src="https://www.w3schools.com/js/pic_bulbon.gif"
+}
+function bulboff(){
+    document.getElementById('firstimg').src="https://www.w3schools.com/js/pic_bulboff.gif"
+}
+var b=false
+function bulbonoff(){
+    if(b==false){
+        document.getElementById('secondimg').src="https://www.w3schools.com/js/pic_bulbon.gif"
+        b=true 
+    }
+    else{
+        document.getElementById('secondimg').src="https://www.w3schools.com/js/pic_bulboff.gif"
+        b=false  
+    }
+}
+var c=false
+function bulbonoff1(){
+    if(c==false){
+        document.getElementById('thirdimg').src="https://www.w3schools.com/js/pic_bulbon.gif"
+        c=true 
+    }
+    else{
+        document.getElementById('thirdimg').src="https://www.w3schools.com/js/pic_bulboff.gif"
+        c=false  
+    }
+}
